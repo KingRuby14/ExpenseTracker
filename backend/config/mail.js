20c3bb8f0b54069d47953bd501a6be8b73998689
samsonking14 <samson2049454@gmail.com>
2026-01-07 22:03:33 +0530

    family

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -1,24 +1,14 @@
-// const nodemailer = require("nodemailer");
-
-// const transporter = nodemailer.createTransport({
-//   service: "gmail",
-//   auth: {
-//     user: process.env.EMAIL_USER,
-//     pass: process.env.EMAIL_PASS, // APP PASSWORD
-//   },
-// });
-
-// module.exports = transporter;
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   },
+  family: 4,   // <<< FORCE IPv4
   tls: {
     rejectUnauthorized: false
   }
