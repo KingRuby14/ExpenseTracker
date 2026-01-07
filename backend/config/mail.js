1a38c76efefb6874bf55964a94867e7121a37adf
samsonking14 <samson2049454@gmail.com>
2026-01-07 22:06:09 +0530

    lasthope

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -3,15 +3,15 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
-  secure: true,
+  secure: true,          // IMPORTANT SSL
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   },
-  family: 4,   // <<< FORCE IPv4
   tls: {
     rejectUnauthorized: false
-  }
+  },
+  connectionTimeout: 20000
 });
 
 module.exports = transporter;
