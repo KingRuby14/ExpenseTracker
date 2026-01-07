fa3a8003fcde480a04fdd9a92eb9852bfebac97a
samsonking14 <samson2049454@gmail.com>
2026-01-07 15:50:54 +0530

    465 port added

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -1,11 +1,13 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,          // IMPORTANT
+  secure: true,       // IMPORTANT (SSL)
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS, // APP PASSWORD
-  },
+    pass: process.env.EMAIL_PASS   // Gmail APP PASSWORD only
+  }
 });
 
 module.exports = transporter;
