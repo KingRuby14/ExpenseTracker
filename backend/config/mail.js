e4dff82026889c215087f2a4f0d2684039269488
samsonking14 <samson2049454@gmail.com>
2026-01-07 14:32:11 +0530

    old email

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -1,12 +1,15 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
+  host: "smtp.gmail.com",
   port: 587, // MUST BE 587 ON RENDER
   secure: false, // TLS STARTTLS
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
