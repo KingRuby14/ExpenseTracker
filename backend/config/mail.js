1345e7b2baac66cd022952ceaa1267017ddf9ffd
samsonking14 <samson2049454@gmail.com>
2026-01-07 21:42:11 +0530

    smtpgmailcom added

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -1,11 +1,27 @@
+// const nodemailer = require("nodemailer");
+
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS, // APP PASSWORD
+//   },
+// });
+
+// module.exports = transporter;
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS, // APP PASSWORD
+    pass: process.env.EMAIL_PASS
   },
+  tls: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = transporter;
