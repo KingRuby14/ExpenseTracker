a751fbd807196d78384bbdaa9ca9825b4988cf69
samsonking14 <samson2049454@gmail.com>
2026-01-07 14:43:00 +0530

    working email

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -1,15 +1,10 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587, // MUST BE 587 ON RENDER
-  secure: false, // TLS STARTTLS
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-  tls: {
-    rejectUnauthorized: false,
+    pass: process.env.EMAIL_PASS, // APP PASSWORD
   },
 });
 
